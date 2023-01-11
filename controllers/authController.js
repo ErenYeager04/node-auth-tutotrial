edc8ff42778a5cf7dfbe5bdd39a3e351537cf508
ErenYeager04 <parkerviktorov094@gmail.com>
2023-01-11 16:47:22 +0100

    end of tutorial 12

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken')
 
 // handle errors
 const handleErrors = (err) => {
@@ -18,6 +19,13 @@ if(err.message.includes('user validation failed')) {
   });
  }
  return errors;
+};
+
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, 'net ninja secret', {
+    expiresIn: maxAge
+  })
 }
 
 
@@ -36,8 +44,10 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
-  }
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000})
+    res.status(201).json({user: user._id});
+  } 
   catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
